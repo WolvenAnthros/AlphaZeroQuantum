96c2819c425fff330eb567b4fc98d67e5f6ade4b
WolvenAnthros <mas_135@mail.ru>
2023-07-04 20:12:56 +0300

    Moved from matplotlib to plotly

--- lib/simulation.py
+++ lib/simulation.py
@@ -1,6 +1,8 @@
 import math
 import numpy as np
-import matplotlib.pyplot as plt
+import plotly.graph_objects as go
+from logger import logger as logs
+from simulation_speed import reward_calculation, wait_calculation
 from lib.args import args
 import re
 
@@ -45,55 +47,50 @@ class Psi:
 Initial params
 '''
 config = args['qbit_simulation_config']
+dimensions = config['n_dimensions']
+omega_01 = config['omega_01']
+omega_osc = config['omega_osc']
+mu = config['mu']
+pulse_time = config['pulse_time']
+pulse_period = 2 * np.pi / omega_osc
+counts = 10
+tau = pulse_period / counts
+
+I = np.identity(dimensions)  # Identity matrix
+annihilation = Operator(dimensions, 0).matrix  # annihilation/creation operators
+creation = Operator(dimensions, 1).matrix
+
+# Finding eigenvalues and eigenvectors
+Hamiltonian_0 = omega_01 * np.matmul(creation, annihilation) - mu / 2 * np.matmul(creation, annihilation) * (
+        np.matmul(creation, annihilation) - I)
+eigenenergy, eigenpsi = np.linalg.eig(Hamiltonian_0)
+
+# initial excited/ground state
+excited_state = np.array(eigenpsi[:, [1]])
+ground_state = np.array(eigenpsi[:, [0]])
 
 
 def simulation(
         pulse_list=None,
-        dimension=config['n_dimensions'],
-        omega_01=config['omega_01'],
-        omega_osc = config['omega_osc'],
-        amp=config['amp'],
-        pulse_time=config['pulse_time'],
-        mu=config['mu'],
+        amp=4,
+        pulse_time=config['pulse_time']
 ):
-
     if pulse_list is None:
-        pulse_list = [0, 0, 0]
+        raise ValueError("No pulse list provided")
 
-    I = np.identity(dimension)  # Identity matrix
-    annihilation = Operator(dimension, 0).matrix  # annihilation/creation operators
-    creation = Operator(dimension, 1).matrix
-    psi = Psi(dimension).matrix  # create psi matrix
-
-    # Finding eigenvalues and eigenvectors
-    Hamiltonian_0 = omega_01 * np.matmul(creation, annihilation) - mu / 2 * np.matmul(creation, annihilation) * (
-            np.matmul(creation, annihilation) - I)
-    eigenenergy, eigenpsi = np.linalg.eig(Hamiltonian_0)
-
-    # initial excited/ground state
-    excited_state = np.array(eigenpsi[:, [1]])
-    ground_state = np.array(eigenpsi[:, [0]])
+    psi = Psi(dimensions).matrix  # create psi matrix
 
     # end_probability_excited|ground|third
     for n in ['excited', 'ground', 'third']:
         globals()['end_probability_%s' % n] = []
-    pulse_period = 2 * np.pi / omega_osc
-    counts = int(pulse_period * config['num_timesteps'] )  # let's take a lot of counts for more reliability
 
-    #tau = pulse_period / counts  # 0.0004
-    tau = pulse_period/counts
     for pulse, index in zip(pulse_list, range(len(pulse_list))):
-        '''
-        Please pay attention that the pulse_period and pulse_time should converge well, otherwise
-        we lose some of the counts
-        '''
+
         # print(f'Pulse:{pulse}, index: {index}')
         for k in range(counts):
+
             # Start of psi calculation
             t = lambda k_: k_ * tau
-            '''
-            Defining pulse shape in time
-            '''
             if t(k) < pulse_time:
                 oscillatory_part = amp * pulse
                 oscillatory_part_1st_derivative = 0
@@ -121,61 +118,149 @@ def simulation(
             psi = fraction @ psi
 
             # excited probability formula
-            probability_excited = excited_state.transpose() @ psi.conjugate()
+            probability_excited = excited_state.T.conjugate() @ psi
             probability_excited = abs(np.sum(probability_excited)) ** 2
             end_probability_excited.append(probability_excited)
 
             # ground probability formula
-            probability_ground = ground_state.transpose() @ psi.conjugate()
+            probability_ground = ground_state.T.conjugate() @ psi
             probability_ground = abs(np.sum(probability_ground)) ** 2
             end_probability_ground.append(probability_ground)
 
-            if dimension >= 3:
+            if dimensions >= 3:
                 third_state = np.array(eigenpsi[:, [2]])
 
                 # third state probability formula
-                probability_third = third_state.transpose() @ psi.conjugate()
+                probability_third = third_state.T.conjugate() @ psi
                 probability_third = abs(np.sum(probability_third)) ** 2
                 end_probability_third.append(probability_third)
-                if abs(probability_excited - 0.5) < 0.015:
-                    print(f'time: {t(k)}, count: {k}')
+
+                if abs(probability_excited - 0.5) < 0.025:
+                    # print(f'time: {t(k)}, count: {k}')
                     leakage = 0
-                    for dim in range(2, dimension):
+                    for dim in range(2, dimensions):
                         high_state = np.array(eigenpsi[:, [dim]])
                         # third state probability formula
                         probability_high = high_state.transpose() @ psi.conjugate()
                         probability_high = abs(np.sum(probability_high)) ** 2
                         leakage += probability_high
-                    print(f'leakage: {leakage}')
-                    print('\n')
-                    return 1, end_probability_ground, end_probability_excited, end_probability_third
+
+
+                    # return 1, end_probability_ground, end_probability_excited, end_probability_third
 
             else:
                 probability_third = 0
                 end_probability_third.append(probability_third)
+
+    logs.info(f'leakage: {leakage:.2e}')
 
     return abs(probability_excited * 2), end_probability_ground, end_probability_excited, end_probability_third
 
 
 def plot_show(ground,
               excited,
-              third,
-              omega_osc=25 * 2 * np.pi):
-    pulse_period = 2 * np.pi / omega_osc
-    counts = int(pulse_period * 2500)
-    tau = pulse_period / counts  # 0.0004
+              third, fidelity_):
+    axis = [x / len(excited) * 2 * np.pi / omega_osc * len(pulse_list) for x in range(len(excited))]
 
-    axis = [x * tau for x in range(len(excited))]
-    fig, at = plt.subplots()
-    at.plot(axis, excited, label='excited state')
-    at.plot(axis, ground, label='ground state')
-    at.plot(axis, third, label='third state')
-    at.set_xlabel('time, ns')
-    at.set_ylabel('probability')
-    at.set_title("Leakage graph")
-    # at.legend(loc='lower left')
-    # plt.yscale("log")
-    plt.show()
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(
+        x=axis,
+        y=ground,
+        mode='lines',
+        name='ground state'
+    ))
+    fig.add_trace(go.Scatter(
+        x=axis,
+        y=excited,
+        mode='lines',
+        name='excited state'
+    ))
+    fig.add_trace(go.Scatter(
+        x=axis,
+        y=third,
+        mode='lines',
+        name='leakage'
+    ))
+    leakage_annotation = [dict(
+        x=axis[-1],
+        y=third[-1],
+        font=dict(size=16),
+        text=f"Leakage: {third[-1]:.2e}",
+        showarrow=True,
+        arrowhead=2,
+        arrowsize=1,
+        arrowwidth=1.8,
+        ay=-80,
+        ax=-100,
+        bordercolor="#c7c7c7",
+        borderwidth=2,
+        borderpad=4,
+        bgcolor="#ff7f0e",
+        opacity=1,
+    )]
+    infidelity_annotation = [dict(
+        text=f"Infidelity: {1 - fidelity_:.2e}",
+        font=dict(size=16),
+        bordercolor="#c7c7c7",
+        borderwidth=2,
+        bgcolor="#ff7f0e",
+        xref="paper", yref="paper",
+        x=0.05, y=0.8,
+        showarrow=False
+    )]
+
+    fig.update_layout(
+        updatemenus=[
+            dict(buttons=list([
+                dict(label="None",
+                     method="update",
+                     args=[{"visible": [True]},
+                           {"title": "States dynamics plot", "annotations": []}]),
+                dict(label="Leakage",
+                     method="update",
+                     args=[{"visible": [True]},
+                           {"title": "Leakage display", "annotations": leakage_annotation}]),
+                dict(label="Infidelity",
+                     method="update",
+                     args=[{"visible": [True]},
+                           {"title": "Infidelity display", "annotations": infidelity_annotation}]),
+                dict(label="Leakage + Infidelity",
+                     method="update",
+                     args=[{"visible": [True]},
+                           {"title": "Infidelity and leakage display",
+                            "annotations": leakage_annotation + infidelity_annotation}]),
+            ]),
+            )
+        ]
+    )
+
+    fig.update_layout(
+        xaxis=dict(
+            showline=True,
+            linewidth=1,
+            linecolor='rgb(0, 0, 0)',
+            showgrid=True,
+            showticklabels=True,
+            ticks='outside'
+        ),
+        yaxis=dict(
+            showline=True,
+            linewidth=1,
+            linecolor='rgb(0, 0, 0)',
+            showgrid=True,
+            showticklabels=True,
+            ticks='outside'
+        ),
+        autosize=False,
+        width=1000,
+        height=700,
+        plot_bgcolor='white'
+    )
+    fig.update_layout(title='State dynamic graph',
+                      xaxis_title='Time, ns',
+                      yaxis_title='Probability')
+    plot_config = {'scrollZoom': True}
+    fig.show(config=plot_config)
 
 
 num_timestemps = config['num_timesteps']
@@ -183,23 +268,23 @@ num_timestemps = config['num_timesteps']
 
 def pulse_show(pulse_list):
     pulse_period = 2 * np.pi / config['omega_osc']
-    tau = pulse_period / num_timestemps
-    axis = [x * tau for x in range(len(pulse_list) * num_timestemps)]
 
-    pulses = []
-    for pulse in pulse_list:
-        pulse_timed = [pulse * config['amp'] if x < config['pulse_time'] else 0 for x in range(num_timestemps)]
-        pulses += pulse_timed
-    fig, at = plt.subplots()
-    at.plot(axis, pulses, label='pulses')
-    at.set_xlabel('time, ns')
-    at.set_ylabel('amplitude')
-    plt.show()
+    axis = [x * pulse_period for x in range(len(pulse_list))]
+    positive_pulse_list = [pulse if pulse > 0 else 0 for pulse in pulse_list]
+    negative_pulse_list = [pulse if pulse < 0 else 0 for pulse in pulse_list]
+    fig = go.Figure(data=
+                    [go.Bar(x=axis, y=negative_pulse_list),
+                     go.Bar(x=axis, y=positive_pulse_list),
+                     ]
+                    )
+
+    plot_config = {'scrollZoom': True}
+    fig.show(config=plot_config)
 
 
 if __name__ == '__main__':
 
-    pulse_str = config['example_scallop']
+    pulse_str = config['example_scallop']  # leakage ~10^-4
     pulse_str = pulse_str.replace('1', '1,')
     pulse_str = pulse_str.replace('0', '0,')
     pulse_list = pulse_str.split(',')
@@ -211,8 +296,17 @@ if __name__ == '__main__':
         pulses_str += pulse
 
     pulse_list = [int(pulse) for pulse in pulse_list]
-    print(pulses_str)
-    win, end_probability_ground, end_probability_excited, end_probability_third = simulation(pulse_list=pulse_list)
-    print(f'Reward: {win:.3e}')
-    plot_show(ground=end_probability_ground, excited=end_probability_excited, third=end_probability_third)
-    # pulse_show(pulse_list)
+    print(pulse_list)
+
+    _, end_probability_ground, end_probability_excited, end_probability_third = simulation(pulse_list=pulse_list)
+
+    fidelity = reward_calculation(pulse_list=pulse_list)
+    logs.debug(
+        f'Fidelity:{fidelity:.4g}')
+    logs.critical(
+        f'Infidelity:{1 - fidelity:.3e}')
+    logs.info(len(pulse_list))
+
+    plot_show(ground=end_probability_ground, excited=end_probability_excited, third=end_probability_third,
+              fidelity_=fidelity)
+    pulse_show(pulse_list)
